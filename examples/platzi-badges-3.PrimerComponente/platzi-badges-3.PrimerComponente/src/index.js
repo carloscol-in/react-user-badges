fda69ef0f6b59a3d7991b963c1686e22d1777d8f
Carlos Colin <contact@carloscolin.me>
2021-06-07 13:17:14 -0500

    user badges using React and Gravatar from Platzi course

--- examples/platzi-badges-3.PrimerComponente/platzi-badges-3.PrimerComponente/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import Badge from './components/Badge';
-
-const container = document.getElementById('app');
-
-ReactDOM.render(<Badge />, container);
